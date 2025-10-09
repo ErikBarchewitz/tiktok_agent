82561004fefae8facec50755f0b8c15a66d134f1
ErikBarchewitz <erik.barchewitz@googlemail.com>
2025-10-09 13:39:18 +0200

    Delete gwo_training directory

--- gwo_training/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import survey_inherit
-from . import gwo_course
-from . import gwo_content
-from . import gwo_enrollment
-from . import gwo_result
--- gwo_training/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import launch_exam_wizard
